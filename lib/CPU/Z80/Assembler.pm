c3e31f417cefcce07c0cbfe2dd7043f32e7fd97a
Paulo Custodio <pauloscustodio@gmail.com>
2009-08-11 14:35:22 +0000

    New list object passed to bytes() to generate assembly listing. Dead code removed.

--- lib/CPU/Z80/Assembler.pm
+++ lib/CPU/Z80/Assembler.pm
@@ -7,8 +7,8 @@ use warnings;
 use 5.008;
 
 use CPU::Z80::Assembler::Lexer;
-use CPU::Z80::Assembler::Parser;
 use CPU::Z80::Assembler::Program;
+use CPU::Z80::Assembler::List;
 
 use Data::Dump qw( dump );
 use HOP::Stream qw( drop head );
@@ -16,7 +16,7 @@ use Text::Tabs; 						# imports expand(), unexpand()
 
 use vars qw(@EXPORT $verbose);
 
-our $VERSION = '2.05_02';
+our $VERSION = '2.05_03';
 
 use base qw(Exporter);
 
@@ -33,170 +33,16 @@ my @warnings;
 
 sub z80asm {
 	my(@input) = @_;
-	my $token_stream = z80lexer(@input);
+	my $input = z80lexer(@input);
+	my $list_output = ($CPU::Z80::Assembler::verbose) ? 
+					CPU::Z80::Assembler::List->new(input => $input, output => \*STDOUT) :
+					undef;
 	my $program = CPU::Z80::Assembler::Program->new();
-	$program->parse($token_stream);
+	$program->parse($input);
 	$program->link;
-	my $bytes = $program->bytes;
+	my $bytes = $program->bytes($list_output);
+	$list_output->flush() if $list_output;
 	return $bytes;
-}
-
-sub old_z80asm {
-    my $source = z80parser(z80lexer(@_));		# input stream
-
-	# initialize
-	%labels = (org => 0);
-	$code = chr(0) x 65536;
-
-    for ($pass = 1; $pass <= 2; $pass++) {
-		my $input = $source;		# input tokens twice
-		
-		# initialize
-	    $address = $startaddr = $maxaddr = 0x0000;
-		
-		my $line;								# last line processed
-		while (my $token = head($input)) {
-			$line = $token->line;
-			my $instr_to_print = expand($line->text);			
-			if ($verbose && $pass == 2) {
-				$instr_to_print =~ s/\s+$//;
-				substr($instr_to_print, 34) = ' ...'
-					if(length($instr_to_print) > 37);
-				printf("0x%04X: %-38s | ", $address, $instr_to_print);
-			}
-
-			# process all instructions in same line
-			$bytes_this_instr = 0;
-			while (($token = head($input)) && $token->line == $line) {
-				@warnings = ();
-				eval { $input = _assemble_instr($input) };
-				if ($@) {					# Semantic error 
-					$line->error($@);
-					die; # unreached
-				}
-				for (@warnings) {			# Warnings
-					$line->warning($_);
-				}
-			}
-			if ($verbose && $pass == 2) {
-				print "\n";
-			}
-		}
-    }
-    return substr($code, $startaddr, $maxaddr - $startaddr);
-}
-
-my %assemble_table = (
-	OPCODE	=> \&_OPCODE,
-	LABEL	=> \&_LABEL,
-	ORG		=> \&_ORG,
-);
-
-sub _assemble_instr {
-	my($input) = @_;
-
-    my $token = drop($input) or return undef;
-    my($type, $value) = @$token;
-
-	my $handler = $assemble_table{$type};
-	if (defined($handler) && ref($handler) eq "CODE") {
-		$handler->($token);
-	}
-	else {
-		die(sprintf("Invalid instruction near 0x%04X: %s\n", 
-					$address, $type));
-    }
-    $maxaddr = $address;
-    return $input;
-}
-
-sub _OPCODE {								# [OPCODE, byte, byte, ...]
-	my($token) = @_;
-	my @bytes = @$token; shift @bytes;		# drop OPCODE
-
-	# compute expressions only on pass 2
-	if ($pass != 2) {
-		$address += scalar(@bytes);
-	}
-	else {
-		my @computed;
-		for (@bytes) {
-			if (ref($_)) {					# [] for empty placeholder, [type, expr] for expression
-				next unless @$_;
-				my($type, $expr) = @$_;
-				my $value = eval_expr($expr, $address, \%labels);
-				if ($type eq "sb") {
-					push(@warnings, "Signed byte $value out of range\n")
-						if ($value >= 0x80 || $value < -0x80);
-					push(@computed, $value & 0xFF);
-				}
-				elsif ($type eq "ub") {
-					push(@warnings, "Unsigned byte $value out of range\n")
-						if ($value >= 0x100 || $value < -0x80);
-					push(@computed, $value & 0xFF);
-				}
-				elsif ($type eq "w") {
-					push(@warnings, "Word $value out of range\n")
-						if ($value >= 0x10000 || $value < -0x8000);
-					push(@computed, $value & 0xFF, ($value >> 8) & 0xFF);
-				}
-				else {
-					die "unknown type $type";
-				}
-			}
-			else {
-				push(@computed, $_ & 0xFF);
-			}
-		}
-		# modify $address only once, so that expressions with '$' use the address of start of instruction
-		$address = _write($address, @computed);
-	}
-}
-
-sub _ORG {
-	my($token) = @_;
-	my $expr = $token->[1];
-	my $value = eval_expr($expr, $address, \%labels);
-	if ($address == $startaddr) {
-		$labels{org} = $address = $startaddr = $value;
-	}
-	elsif ($value != $address) {
-		die "ORG cannot change address\n";
-	}
-	else {
-		# OK
-	}
-}
-
-sub _LABEL {
-	my($token) = @_;
-	my($type, $name, $expr) = @$token;
-	if ($expr) {
-		$labels{$name} = $expr;				# LABEL = EXPRESSION --> save expression
-	}
-	else {
-		$labels{$name} = $address;			# LABEL: --> save address
-	}
-}
-
-#------------------------------------------------------------------------------
-# $address = _write($address, @bytes)
-#	Write bytes, return new address
-sub _write {
-    my($address, @bytes) = @_;
-    for my $byte (@bytes) {
-		$bytes_this_instr++;
-		if ($pass == 2) {
-			$byte &= 0xFF;
-			substr($code, $address, 1) = chr($byte);
-			if ($verbose) {
-				print "\n".(' ' x 47).'| ' if ($bytes_this_instr && !($bytes_this_instr % 10));
-				printf("%02X ", $byte);
-			}
-		}
-		$address++;
-	}
-	return $address;
 }
 
 1;
