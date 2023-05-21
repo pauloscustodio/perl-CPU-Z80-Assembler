7ee039061c9e58e1a5b0186da2f405b0bcf2bbb5
Paulo Custodio <pauloscustodio@gmail.com>
2023-05-21 18:51:55 +0100

    Bug #140160 for CPU-Z80-Assembler: circular dependency in CPU::Z80::Assembler::List fails syntax check. https://rt.cpan.org/Public/Bug/Display.html?id=140160

--- tools/build_Parser.pl
+++ tools/build_Parser.pl
@@ -22,7 +22,7 @@ my($MODULE, $FILE) = @ARGV;
 my $g = ParserGenerator->new;
 
 $g->prolog(q{
-our $VERSION = '2.18';
+our $VERSION = '2.19';
 
 use CPU::Z80::Assembler;
 use CPU::Z80::Assembler::Expr;
